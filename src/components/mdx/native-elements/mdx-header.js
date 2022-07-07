85e8290347f1da202675faa8c7e0835cac312e04
John D'Oriocourt <kibner@gmail.com>
2022-07-07 16:47:12 -0500

    fixed issue with incorrect scroll height for headers

--- src/components/mdx/native-elements/mdx-header.js
+++ src/components/mdx/native-elements/mdx-header.js
@@ -2,38 +2,45 @@ import withStyles from "@mui/styles/withStyles"
 import { Typography } from "@mui/material"
 import React from "react"
 
-const StyledHeader = withStyles(theme => ({
-  root: {
-    marginBottom: theme.spacing(1),
-    scrollMarginTop: `${theme.mixins.toolbar.minHeight + theme.spacing(1)}px`,
-    "@media (min-width:0px) and (orientation: landscape)": {
-      scrollMarginTop: `${
-        theme.mixins.toolbar[
-          "@media (min-width:0px) and (orientation: landscape)"
-          ].minHeight + theme.spacing(1)
-      }px`
-    },
-    "@media (min-width:600px)": {
-      scrollMarginTop: `${
-        theme.mixins.toolbar["@media (min-width:600px)"].minHeight +
-        theme.spacing(1)
-      }px`
-    },
-    "&:hover > a.anchor > svg": {
-      visibility: "visible"
-    },
-    "&>a": {
-      fill: theme.palette.text.secondary,
-      position: "absolute",
-      transform: "translateX(-100%)",
-      paddingRight: theme.spacing(0.5),
-      "&>svg": {
-        visibility: "hidden",
-        verticalAlign: "text-top"
+const StyledHeader = withStyles(theme => {
+  const singleSpacingPixels = parseFloat(theme.spacing(1))
+
+  return (
+    {
+      root: {
+        marginBottom: theme.spacing(1),
+        scrollMarginTop: `${
+          theme.mixins.toolbar.minHeight
+          + singleSpacingPixels}px`,
+        "@media (min-width:0px) and (orientation: landscape)": {
+          scrollMarginTop: `${
+            theme.mixins.toolbar[
+              "@media (min-width:0px) and (orientation: landscape)"
+              ].minHeight + singleSpacingPixels
+          }px`
+        },
+        "@media (min-width:600px)": {
+          scrollMarginTop: `${
+            theme.mixins.toolbar["@media (min-width:600px)"].minHeight
+            + singleSpacingPixels
+          }px`
+        },
+        "&:hover > a.anchor > svg": {
+          visibility: "visible"
+        },
+        "&>a": {
+          fill: theme.palette.text.secondary,
+          position: "absolute",
+          transform: "translateX(-100%)",
+          paddingRight: theme.spacing(0.5),
+          "&>svg": {
+            visibility: "hidden",
+            verticalAlign: "text-top"
+          }
+        }
       }
-    }
-  }
-}))(Typography)
+    })
+})(Typography)
 
 const MdxHeader = props => <StyledHeader {...props} />
 

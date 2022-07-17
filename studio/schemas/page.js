aee0607c813d7f9948d8450d61f4e2835658925a
John D'Oriocourt <kibner@gmail.com>
2022-07-16 20:39:18 -0500

    added a basic Page content schema

---
+++ studio/schemas/page.js
@@ -0,0 +1,23 @@
+export default {
+  name: "page",
+  type: "document",
+  title: "Page",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string"
+    },
+    {
+      name: "display_order",
+      title: "Display Order",
+      type: "number"
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "array",
+      of: [{ type: "block" }]
+    }
+  ]
+}

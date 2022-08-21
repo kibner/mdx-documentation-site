0e402dc54965b0d5c8194e9e7cbed2d5b8192eb3
John D'Oriocourt <kibner@gmail.com>
2022-08-20 19:39:43 -0500

    updated packages and some configuration in order to support new version of gatsby mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,21 +34,23 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-autolink-headers`,
-            options: {},
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 960,
+        mdxOptions: {
+          remarkPlugins: [
+            {
+              resolve: `gatsby-remark-autolink-headers`,
+              options: {},
             },
-          },
-        ],
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                // It's important to specify the maxWidth (in pixels) of
+                // the content container as this plugin uses this as the
+                // base for generating different widths of each image.
+                maxWidth: 960,
+              },
+            },
+          ],
+        },
       },
     },
     `gatsby-theme-material-ui`,

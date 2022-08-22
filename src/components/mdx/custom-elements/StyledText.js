f3c2f879ef09b93aa0eed01f07e210da2eec5ae8
John D'Oriocourt <kibner@gmail.com>
2022-08-21 22:10:52 -0500

    added a new component to let users style text in mdx files

---
+++ src/components/mdx/custom-elements/StyledText.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const StyledText = props => <span style={props.style}>{props.children}</span>
+
+export default StyledText
--- src/templates/configs/mdx-shortcodes.js
+++ src/templates/configs/mdx-shortcodes.js
@@ -4,6 +4,7 @@ import D3 from "../../components/mdx/custom-elements/icons/dice/d3"
 import D4 from "../../components/mdx/custom-elements/icons/dice/d4"
 import D5 from "../../components/mdx/custom-elements/icons/dice/d5"
 import D6 from "../../components/mdx/custom-elements/icons/dice/d6"
+import StyledText from "../../components/mdx/custom-elements/StyledText"
 
 const MdxShortcodes = {
   D1,
@@ -12,6 +13,7 @@ const MdxShortcodes = {
   D4,
   D5,
   D6,
+  StyledText,
 }
 
 export default MdxShortcodes
